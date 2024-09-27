0fa256b755538ff97ca65dfa178136254e0a3fc0
BogdanSotnikov <bogdans2789>
2024-09-27 11:46:20 -0400

    Added test cases.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,6 @@
 public class MyArrays
 {
-  public String arrayToString(int[] nums)
+  public static String arrayToString(int[] nums)
   {
     String s = "[";
 
@@ -13,8 +13,38 @@ public class MyArrays
     return s+"]";
   }
 
+  public static int[] returnCopy(int[]ary)
+  {
+    int[] out = new int[ary.length];
+    return out;
+  }
+
+  public static int[] concatArray(int[]ary1,int[]ary2)
+  {
+    int[] out = new int[ary1.length+ary2.length];
+    return out;
+  }
+
   public static void main(String[] args)
   {
-    
+    //returnCopy tests
+    int[] arr = new int[] {1, 2, 3, 4, 5};
+    System.out.println("returnCopy tests:");
+    System.out.println(arrayToString(returnCopy(arr)).equals(arrayToString(arr)));
+    arr = new int[0];
+    System.out.println(arrayToString(returnCopy(arr)).equals(arrayToString(arr)));
+    arr = new int[] {4, 4, 4, 4, 4};
+    System.out.println(arrayToString(returnCopy(arr)).equals(arrayToString(arr)));
+    arr = new int[] {0};
+    System.out.println(arrayToString(returnCopy(arr)).equals(arrayToString(arr)));
+
+    //concatArray tests
+    int[] arr1 = new int[] {1, 2, 3};
+    int[] arr2 = new int[] {4, 5, 6};
+    System.out.println("concatArray tests:");
+    System.out.println(arrayToString(concatArray(arr1, arr2)).equals(arrayToString(arr1)+arrayToString(arr2)));
+    arr1 = new int[0];
+    arr2 = new int[0];
+    System.out.println(arrayToString(concatArray(arr1, arr2)).equals(arrayToString(arr1)+arrayToString(arr2)));
   }
 }
