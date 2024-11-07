c55eb2dae423d31d599378705926bcf021f2d29f
BogdanSotnikov <bogdans2789@nycstudents.net>
2024-11-06 21:02:30 -0500

    Added working countTrianglesB

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -24,7 +24,40 @@ public class TriangleTester
       return count;
     }
 
+    public static int countTrianglesB(String filename)
+    {
+      int count = 0;
+      try
+      {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine() && input.hasNextInt())
+        {
+          int[] col1 = new int[3];
+          int[] col2 = new int[3];
+          int[] col3 = new int[3];
+          col1[0] = input.nextInt();
+          col2[0] = input.nextInt();
+          col3[0] = input.nextInt();
+          col1[1] = input.nextInt();
+          col2[1] = input.nextInt();
+          col3[1] = input.nextInt();
+          col1[2] = input.nextInt();
+          col2[2] = input.nextInt();
+          col3[2] = input.nextInt();
 
+          count += isTriangle(col1[0],col1[1],col1[2]);
+          count += isTriangle(col2[0],col2[1],col2[2]);
+          count += isTriangle(col3[0],col3[1],col3[2]);
+        }
+        input.close();
+      }
+      catch (FileNotFoundException ex)
+      {
+        System.out.println("File not found");
+      }
+      return count;
+    }
 
     public static int isTriangle(int a, int b, int c)
     {
@@ -35,6 +68,7 @@ public class TriangleTester
 
     public static void main (String[] args)
     {
-      System.out.println(countTrianglesA("inputTri.txt"));
+      System.out.println("Number of triangles by countTrianglesA: "+countTrianglesA("inputTri.txt"));
+      System.out.println("Number of triangles by countTrianglesB: "+countTrianglesB("inputTri.txt"));
     }
 }
