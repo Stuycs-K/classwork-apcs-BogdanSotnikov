e92e50d5629a6123a491f5673838151e755e7bdb
BogdanSotnikov <bogdans2789>
2024-11-07 11:29:26 -0500

    Overrode speak() for bird

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -3,5 +3,7 @@ public class Driver
   public static void main (String[] args){
     Animal a = new Animal("heheheha",66,"Alpha");
     a.speak();
+    Bird b = new Bird("squawk",1,"Jim",12,"black");
+    b.speak();
   }
 }
