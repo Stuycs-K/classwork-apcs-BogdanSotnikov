b68a24da75b7221ac5712878ff2eba4cd2f44543
BogdanSotnikov <bogdans2789>
2024-11-07 11:45:29 -0500

    Completed steps 9-10

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,7 +1,9 @@
 /* Now, whenever you call getName() of bird, it returns "The Mighty " + Name
 Before, it used to just return Name
-When an inherited method calls an overridden method, the overridden method will work inside the inherited method when the inherited method is called. 
-*/
+When an inherited method calls an overridden method, the overridden method will work inside the inherited method when the inherited method is called.*/
+/*b2 did not work because you cannot create an Animal that uses the Bird's methods.
+If Bird has methods that Animal doesn't, that would create issues
+Intuitively, not all animals are birds.*/
 public class Driver
 {
   public static void main (String[] args){
@@ -9,5 +11,10 @@ public class Driver
     a.speak();
     Bird b = new Bird("squawk",1,"Jim",12,"black");
     b.speak();
+
+    Animal a1 = new Animal("K", 1, "G");
+    Bird b1 = new Bird("K", 1, "G", 30, "A");
+    //Bird b2 = new Animal("K", 1, "G"); <- incompatible types: Animal cannot be converted to Bird
+    Animal a2 = new Bird("K", 1, "G", 30, "A");
   }
 }
