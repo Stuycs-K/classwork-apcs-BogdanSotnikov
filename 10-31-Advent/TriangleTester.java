74cb6bd21d6bbbadaadcb3def466b69128e17faf
BogdanSotnikov <bogdans2789@nycstudents.net>
2024-11-06 20:51:45 -0500

    Made working countTrianglesA

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -2,35 +2,39 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TriangleTester{
-    public static int countTrianglesA(String filename){
-    int count = 0; 
-      try {
-      File file = new File(filename);
-      Scanner input = new Scanner(file);
-      while (input.hasNextLine()){
-      String line = input.nextLine;
-        for(int i = 0 ; i < line.length(); i ++){
-          count += isTriangle(line.nextInt(),line.nextInt(),line.nextInt());
+public class TriangleTester
+{
+    public static int countTrianglesA(String filename)
+    {
+      int count = 0;
+      try
+      {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine() && input.hasNextInt())
+        {
+          count += isTriangle(input.nextInt(),input.nextInt(),input.nextInt());
         }
+        input.close();
       }
+      catch (FileNotFoundException ex)
+      {
+        System.out.println("File not found");
       }
-      input.close();
+      return count;
+    }
 
-    } catch (FileNotFoundException ex) {
-      System.out.println("File not found");
-      return;
-      } 
-      return count; 
+
+
+    public static int isTriangle(int a, int b, int c)
+    {
+      if (a+b>c && a+c>b && b+c>a)
+        return 1;
+      return 0;
     }
-    
-    public static int isTriangle(int a, b, c){
-    if (a+b>c && a+c>b && b+c>a)
-      return 1;
-    return 0;
+
+    public static void main (String[] args)
+    {
+      System.out.println(countTrianglesA("inputTri.txt"));
     }
-    
-    public static void main (String[] args){
-    
-  } 
 }
