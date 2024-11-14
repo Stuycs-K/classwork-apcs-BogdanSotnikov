9ef37d9cc1e37f11355f0dc7b86cba77a14eb26d
BogdanSotnikov <bogdans2789>
2024-11-14 10:34:51 -0600

    Added replaceEmpty and its test cases

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -9,14 +9,35 @@ public class ArrayListPractice
     for (int i = 0; i<size; i++)
     {
       Random.add(""+((int)(21*Math.random())));
-      if (Random.get(i) == "0")
+      if (Random.get(i).equals("0"))
         Random.set(i, "");
     }
 
     return Random;
   }
 
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+    for (int i = 0; i < original.size() ; i ++ ) {
+      if(original.get(i).equals("")){
+        original.set(i,"Empty");
+      }
+    }
+  }
+
   public static void main(String[] args) {
+    /*
     System.out.println(createRandomArray(5));
+    System.out.println(createRandomArray(1));
+    System.out.println(createRandomArray(0));
+    System.out.println(createRandomArray(100));
+    */
+
+    //test replaceEmpty
+    ArrayList<String> a1 = createRandomArray(100);
+    System.out.println(a1);
+    replaceEmpty(a1);
+    System.out.println(a1);
   }
 }
