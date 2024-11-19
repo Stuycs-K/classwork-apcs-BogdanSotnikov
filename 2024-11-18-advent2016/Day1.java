007a26cbf09b1a0e7c0b120f295c44e1d987bbec
BogdanSotnikov <bogdans2789>
2024-11-19 10:46:33 -0600

    Did day 1 pt 1, day 3 pts 1 and 2, started day 1 pt 2

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Day1
 {
@@ -14,35 +15,39 @@ public class Day1
     {
       File file = new File(filename);
       Scanner input = new Scanner(file);
-      String[] values = input.NextLine().split(", ");
+      String[] values = input.nextLine().split(", ");
+      int[] checked = new int[values.length];
       for (int i = 0; i<values.length; i++) {
         if (values[i].charAt(0) == 'R')
           facing += 90;
         else
           facing -=90;
         while (facing > 180)
-          facing -= 90;
+          facing -= 360;
         while (facing < -180)
-          facing += 90;
+          facing += 360;
         if (facing == 0)
-          y += (int)values[i].substring(1);
+          y += Integer.parseInt(values[i].substring(1));
         else if (facing == 90)
-          x += (int)values[i].substring(1);
+          x += Integer.parseInt(values[i].substring(1));
         else if (facing == -90)
-          x -= (int)values[i].substring(1);
+          x -= Integer.parseInt(values[i].substring(1));
         else
-          y -= (int)values[i].substring(1);
+          y -= Integer.parseInt(values[i].substring(1));
+        if (Arrays.asList(checked).contains(x+y))
+          System.out.println(Math.abs(x)+Math.abs(y));
+        checked[i] = x+y;
       }
-      return (Math.abs(x)+Math.abs(y));
     }
     catch (FileNotFoundException ex)
     {
       System.out.println("File not found");
     }
+    return (Math.abs(x)+Math.abs(y));
   }
 
   public static void main(String[] args)
   {
-    System.out.println("inputDay1");
+    System.out.println(findDistance("InputDay1.txt"));
   }
 }
