aa7b1a6c02448115e0952cf4491663cdc2444757
BogdanSotnikov <bogdans2789>
2025-01-02 10:44:21 -0600

    fib

---
+++ 2025-01-02/Driver.java
@@ -0,0 +1,15 @@
+class Driver
+{
+  public static void main(String[] args)
+  {
+    System.out.println(fib(Integer.parseInt(args[0])));
+  }
+
+  public static int fib (int n)
+  {
+    if (n<2)
+      return n;
+    else
+      return (fib(n-2)+fib(n-1));
+  }
+}
